22384cb2958edfee60b0d731db924bc287d436f0
Nathan Goh <gohjunhao95@gmail.com>
2024-03-16 17:46:45 +0800

    Completed API calling for required params, added in toggle functions

--- src/app/components/CardInfo.js
+++ src/app/components/CardInfo.js
@@ -1,51 +1,92 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import axios from "axios";
 
 const API_URL = "https://restcountries.com/v3.1/all";
 
 const CardInfo = () => {
-  const [countries, setCountries] = useState([]);
+  const [apiData, setApiData] = useState([]);
+  const [darkMode, setDarkMode] = useState(true);
+  const [listView, setListView] = useState(true);
 
   useEffect(() => {
-    axios
-      .get(API_URL)
-      .then((res) => {
-        const extractedCountries = res.data.map((country) => ({
-          name: country.name.common,
-          population: country.population,
-          region: country.region,
-          capital: country.capital,
-          currency: country.currencies
-            ? Object.keys(country.currencies).map((currencyKey) => ({
-                key: currencyKey,
-                symbol: country.currencies[currencyKey].symbol,
-              }))
-            : [],
-        }));
-        setCountries(res.data);
+    fetch(API_URL)
+      .then((response) => {
+        return response.json();
       })
-      .catch((error) => console.log(error));
+      .then((data) => {
+        setApiData(data);
+      })
+      .catch((error) => console.error(error));
   }, []);
 
+  // To filter the data that I need
+  let countries = [];
+  // Iterate over each data object
+  apiData.map((apiCountry) => {
+    // Check if currencies property exists and it's an object
+    if (apiCountry.currencies && typeof apiCountry.currencies === "object") {
+      // Create variable to store currency from all countries
+      let currencies = [];
+      // Iterate over the keys of currencies object
+      Object.keys(apiCountry.currencies).forEach((currencyKey) => {
+        // Push currency symbol and value into respective arrays, if no currency symbol then only display currency key
+        if (apiCountry.currencies[currencyKey].symbol) {
+          currencies.push(`${apiCountry.currencies[currencyKey].symbol} ${currencyKey}`);
+        } else {
+          currencies.push(`${currencyKey}`);
+        }
+      });
+
+      // Push object containing required params into countries array
+      countries.push({
+        flags: apiCountry.flags.svg,
+        name: apiCountry.name.common,
+        population: apiCountry.population,
+        region: apiCountry.region,
+        capital: apiCountry.capital,
+        currenciesKeys: currencies,
+      });
+    }
+  });
+
+  // Change page mode (dark mode / light mode)
+  const changePageMode = () => {
+    setDarkMode(!darkMode);
+  };
+
+  // Change page view (list / card view)
+  const changePageView = () => {
+    setListView(!listView);
+  };
+
   return (
-    <div>
-      {countries.map((country, index) => (
-        <div key={index}>
-          <h2>{country.name.common}</h2>
-          <p>Population: {country.population}</p>
-          <p>Region: {country.region}</p>
-          <p>Capital: {country.capital}</p>
-          {/* {country.currencies &&
-            country.currencies.map((currency, idx) => (
-              <p key={idx}>
-                {currency.symbol}
-                {currency.key}
-              </p>
-            ))} */}
-        </div>
-      ))}
-    </div>
+    <>
+      {/* header section */}
+      <div>
+        <h1>Countries {listView ? "Table" : "Card"} View</h1>
+      </div>
+      <div onClick={() => changePageMode()}>{darkMode ? "Dark Mode" : "Light Mode"}</div>
+      <div onClick={() => changePageView()}>{listView ? "Table" : "Card"}</div>
+      <div>
+        {countries.map((country, index) => (
+          <div key={index}>
+            <img src={country.flags} class="w-10 h-10" />
+            <h1>{country.name}</h1>
+            <p>Population: {country.population} </p>
+            <p>Region: {country.region} </p>
+            <p>Capital: {country.capital} </p>
+            <p>Currency:</p>
+            <div class="bg-red-500 flex flex-row">
+              {country.currenciesKeys.map((currency, index) => (
+                <div key={index} class="bg-red-100 mr-3">
+                  {currency}
+                </div>
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
   );
 };
 

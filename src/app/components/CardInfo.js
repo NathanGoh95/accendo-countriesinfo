dff61e9097cd3d0f1f82c046bb26dfd06b3717ec
Nathan Goh <gohjunhao95@gmail.com>
2024-03-31 13:07:00 +0800

    WIP for filtering feature

--- src/app/components/CardInfo.js
+++ src/app/components/CardInfo.js
@@ -7,6 +7,8 @@ const CardInfo = () => {
   const [apiData, setApiData] = useState([]);
   const [darkMode, setDarkMode] = useState(true);
   const [listView, setListView] = useState(true);
+  const [selectedRegion, setSelectedRegion] = useState([]);
+  const [filteredCountries, setFilteredCountries] = useState([]);
 
   useEffect(() => {
     fetch(API_URL)
@@ -49,6 +51,16 @@ const CardInfo = () => {
     }
   });
 
+  // Initialize a set to store unique regions
+  const uniqueRegions = new Set();
+  // Iterate over each data object
+  countries.map((regionList) => {
+    // Add the region value to the set
+    uniqueRegions.add(regionList.region);
+  });
+  // Convert the set back to an array to obtain unique regions
+  const uniqueRegionsArray = [...uniqueRegions];
+
   // Change page mode (dark mode / light mode)
   const changePageMode = () => {
     setDarkMode(!darkMode);
@@ -59,6 +71,25 @@ const CardInfo = () => {
     setListView(!listView);
   };
 
+  const onSelectRegion = (value) => {
+    setSelectedRegion(value);
+  };
+
+  const refreshData = () => {
+    if (countries) {
+      setFilteredCountries(
+        countries?.filter((item) => {
+          return selectedRegion === "All" || item.region === selectedRegion;
+        }),
+      );
+    } else {
+      setFilteredCountries(countries);
+    }
+  };
+  useEffect(() => {
+    refreshData();
+  }, [selectedRegion, apiData]);
+
   return (
     <>
       {/* header section */}
@@ -67,24 +98,39 @@ const CardInfo = () => {
       </div>
       <div onClick={() => changePageMode()}>{darkMode ? "Dark Mode" : "Light Mode"}</div>
       <div onClick={() => changePageView()}>{listView ? "Table" : "Card"}</div>
+
+      {/* filter section */}
       <div>
-        {countries.map((country, index) => (
-          <div key={index}>
-            <img src={country.flags} class="w-10 h-10" />
-            <h1>{country.name}</h1>
-            <p>Population: {country.population} </p>
-            <p>Region: {country.region} </p>
-            <p>Capital: {country.capital} </p>
-            <p>Currency:</p>
-            <div class="bg-red-500 flex flex-row">
-              {country.currenciesKeys.map((currency, index) => (
-                <div key={index} class="bg-red-100 mr-3">
-                  {currency}
-                </div>
-              ))}
+        <label>
+          What do we eat?
+          <select value={selectedRegion} onChange={(event) => onSelectRegion(event.target.value)}>
+            {uniqueRegionsArray.map((region) => {
+              return <option value={region}>{region}</option>;
+            })}
+          </select>
+        </label>
+      </div>
+
+      <div>
+        {filteredCountries?.map((country, index) => {
+          return (
+            <div key={index}>
+              <img src={country.flags} className="w-10 h-10" />
+              <h1>{country.name}</h1>
+              <p>Population: {country.population} </p>
+              <p>Region: {country.region} </p>
+              <p>Capital: {country.capital} </p>
+              <p>Currency:</p>
+              <div className="bg-red-500 flex flex-row">
+                {country.currenciesKeys.map((currency, index) => (
+                  <div key={index} className="bg-red-100 mr-3">
+                    {currency}
+                  </div>
+                ))}
+              </div>
             </div>
-          </div>
-        ))}
+          );
+        })}
       </div>
     </>
   );

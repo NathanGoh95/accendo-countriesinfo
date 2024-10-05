56f6038b59180f97cf9fe18304151674887fbf96
Nathan Goh <gohjunhao95@gmail.com>
2024-10-05 22:25:21 +0800

    improve filter UI, add clear filter, clicking outside to close filter feature

--- src/app/components/SearchBar.jsx
+++ src/app/components/SearchBar.jsx
@@ -10,7 +10,7 @@ const SearchBar = observer(() => {
 
   const debounceSearch = debounce((value) => {
     filteredCountryStore.setSearchInput(value);
-  }, 1000);
+  }, 200);
 
   const handleInputChange = (e) => {
     const value = e.target.value;
@@ -27,16 +27,18 @@ const SearchBar = observer(() => {
     <div className="m-1 relative w-[25rem]">
       <div
         className={`relative flex items-center transition-all duration-200 ease-in-out 
-            ${isFocused || input ? "shadow-lg" : "shadow-md hover:shadow-lg"
-        }`}>
+            ${isFocused || input ? "shadow-lg" : "shadow-md hover:shadow-lg"}`}>
         <div className="absolute left-3 w-3 h-3 flex items-center justify-center">
           <LucideSearch className="text-gray-400" />
         </div>
         <input
           type="text"
-          className={`w-full pl-10 pr-8 text-sm border rounded-md outline-none
-            ${isFocused || input ? "border-blue-500 bg-blue-50" : "border-gray-300 hover:border-gray-400"
-          }`}
+          className={`w-full px-10 py-2 text-sm border rounded-md outline-none
+            ${
+              isFocused || input
+                ? "border-blue-500 bg-blue-50"
+                : "border-gray-300 hover:border-gray-400"
+            }`}
           placeholder="Search for a country"
           value={input}
           onChange={handleInputChange}
@@ -44,7 +46,10 @@ const SearchBar = observer(() => {
           onBlur={() => setIsFocused(false)}
         />
         {input && (
-          <button type="button" className="absolute inset-y-0 right-0 flex items-center pr-3" onClick={clearSearch}>
+          <button
+            type="button"
+            className="absolute inset-y-0 right-0 flex items-center pr-3"
+            onClick={clearSearch}>
             <LucideX className="w-3 h-3 text-gray-400 hover:text-gray-800" />
           </button>
         )}

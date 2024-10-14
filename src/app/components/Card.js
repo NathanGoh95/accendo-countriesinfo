903df1aa9d2eeb83ef7aa0acb8c6807dda425758
NathanGoh95 <gohjunhao95@gmail.com>
2024-10-14 20:09:25 +0800

    improve mui cards ui

--- src/app/components/Card.js
+++ src/app/components/Card.js
@@ -1,41 +1,55 @@
-import { Card, CardContent, CardMedia, Typography } from '@mui/material';
+import { Box, Card, CardContent, CardMedia, Typography } from '@mui/material';
 import * as React from 'react';
 
 export const Cards = ({ country, onClick }) => {
   return (
     <Card
       sx={{
-        maxWidth: '250px',
-        maxHeight: '300px',
+        width: '375px',
         cursor: 'pointer',
         display: 'flex',
         flexDirection: 'column',
         justifyContent: 'space-between',
-        borderRadius: '5px',
+        borderRadius: '6px',
         overflow: 'hidden',
         boxShadow: '0 4px 32px rgba(137,194,217, 0.2)',
         transition: 'transform 0.3s ease-in-out, box-shadow 0.3s ease-in-out',
         '&:hover': {
-          transform: 'translateY(-10px)',
+          transform: 'translateY(-5px)',
           boxShadow: '0 8px 16px rgba(137,194,217, 0.4)',
         },
       }}
       onClick={() => onClick(country)}>
-      <CardMedia component='img' sx={{ width: '250px', height: '140px', objectFit: 'cover' }} image={country.flags} alt={`${country.name} flag`} />
+      <CardMedia component='img' sx={{ width: '100%', height: '225px', objectFit: 'cover' }} image={country.flags} alt={`${country.name} flag`} />
 
-      <CardContent>
-        <Typography gutterBottom variant='h6' component='div'>
+      <CardContent sx={{ background: '#f5f5f5', padding: '24px', paddingX: '16px', paddingY: '8px' }}>
+        <Typography variant='h6' component='div' sx={{ fontWeight: 'bold', fontSize: '18px', marginBottom: '4px' }}>
           {country.name}
         </Typography>
-        <Typography variant='body2' color='text.secondary'>
-          Population: {country.population}
-        </Typography>
-        <Typography variant='body2' color='text.secondary'>
-          Region: {country.region}
-        </Typography>
-        <Typography variant='body2' color='text.secondary'>
-          Capital: {country.capital || 'N/A'}
-        </Typography>
+        <Box>
+          <Typography variant='body2' sx={{ fontWeight: 'bold', display: 'inline-block', fontSize: '14px' }}>
+            Population:
+          </Typography>
+          <Typography variant='body2' color='text.secondary' sx={{ display: 'inline-block', marginLeft: '4px', fontSize: '14px' }}>
+            {country.population}
+          </Typography>
+        </Box>
+        <Box>
+          <Typography variant='body2' sx={{ fontWeight: 'bold', display: 'inline-block', fontSize: '14px' }}>
+            Region:
+          </Typography>
+          <Typography variant='body2' color='text.secondary' sx={{ display: 'inline-block', marginLeft: '4px', fontSize: '14px' }}>
+            {country.region}
+          </Typography>
+        </Box>
+        <Box>
+          <Typography variant='body2' sx={{ fontWeight: 'bold', display: 'inline-block', fontSize: '14px' }}>
+            Capital:
+          </Typography>
+          <Typography variant='body2' color='text.secondary' sx={{ display: 'inline-block', marginLeft: '4px', fontSize: '14px' }}>
+            {country.capital || 'N/A'}
+          </Typography>
+        </Box>
       </CardContent>
     </Card>
   );

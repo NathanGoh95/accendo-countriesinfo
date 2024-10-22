e69ee20fc50ee88cacf9ebd05c34e2c77761218e
NathanGoh95 <gohjunhao95@gmail.com>
2024-10-22 21:03:53 +0800

    create modal componant using mui and first version UI styling

--- src/app/components/TableView.jsx
+++ src/app/components/TableView.jsx
@@ -1,14 +1,18 @@
-"use client";
-import React, { useEffect } from "react";
-import { observer } from "mobx-react";
-import { apiStore } from "../store/ApiStore";
-import { filteredCountryStore } from "../store/FilteredCountryStore";
-import { pageModeStore } from "../store/PageModeStore";
-import SearchBar from "./SearchBar";
-import Filter from "./Filter";
-import { Tables } from "./Table";
+'use client';
+import React, { useEffect, useState } from 'react';
+import { observer } from 'mobx-react';
+import { apiStore } from '../store/ApiStore';
+import { filteredCountryStore } from '../store/FilteredCountryStore';
+import { pageModeStore } from '../store/PageModeStore';
+import SearchBar from './SearchBar';
+import Filter from './Filter';
+import { Tables } from './Table';
+import { CountryModal } from './CountryModal';
 
 const TableView = observer(() => {
+  const [openModal, setOpenModal] = useState(false);
+  const [selectedCountry, setSelectedCountry] = useState(null);
+
   useEffect(() => {
     apiStore.callApi();
   }, []);
@@ -17,33 +21,32 @@ const TableView = observer(() => {
     filteredCountryStore.setSelectedRegion(value);
   };
 
+  const handleOpenModal = (country) => {
+    setSelectedCountry(country);
+    setOpenModal(true);
+  };
+
+  const handleCloseModal = (country) => {
+    setOpenModal(false);
+    setSelectedCountry(null);
+  };
+
   return (
     <>
       {/* header section & toggles*/}
       <div>
-        <h1>Countries {pageModeStore.listView ? "Table" : "Card"} View</h1>
+        <h1>Countries {pageModeStore.listView ? 'Table' : 'Card'} View</h1>
       </div>
-      <div onClick={pageModeStore.toggleDarkMode}>{pageModeStore.darkMode ? "Dark Mode" : "Light Mode"}</div>
+      <div onClick={pageModeStore.toggleDarkMode}>{pageModeStore.darkMode ? 'Dark Mode' : 'Light Mode'}</div>
       <SearchBar />
       <Filter />
-      <div onClick={pageModeStore.toggleListView}>{pageModeStore.listView ? "Table" : "Card"}</div>
+      <div onClick={pageModeStore.toggleListView}>{pageModeStore.listView ? 'Table' : 'Card'}</div>
 
       {/* Render countries */}
       <div>
-        {/* {filteredCountryStore.filteredCountries.map((country, index) => {
-          return (
-            <div key={index}>
-              <img src={country.flags} className="w-10 h-10" alt={`${country.name} flag`} />
-              <h1>{country.name}</h1>
-              <p>Population: {country.population} </p>
-              <p>Region: {country.region} </p>
-              <p>Capital: {country.capital} </p>
-              <p>Currency: {country.currencies.join(', ')}</p>
-            </div>
-          );
-        })} */}
-        <Tables countries={filteredCountryStore.filteredCountries} />
+        <Tables countries={filteredCountryStore.filteredCountries} onRowClick={handleOpenModal} />
       </div>
+      <CountryModal open={openModal} handleClose={handleCloseModal} country={selectedCountry} />
     </>
   );
 });

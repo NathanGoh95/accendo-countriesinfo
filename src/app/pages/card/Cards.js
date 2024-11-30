c30d089c930d74a2a79c2661c6bfbf789c813f5e
Nathan Goh <gohjunhao95@gmail.com>
2024-11-30 22:21:42 +0800

    change page name, improve card, modal, table flag and filter ui for consistency

--- src/app/pages/card/Cards.js
+++ src/app/pages/card/Cards.js
@@ -6,6 +6,7 @@ export const Cards = ({ country, onClick }) => {
     <Card
       sx={{
         width: '275px',
+        height: '290px',
         cursor: 'pointer',
         display: 'flex',
         flexDirection: 'column',
@@ -14,41 +15,64 @@ export const Cards = ({ country, onClick }) => {
         overflow: 'hidden',
         boxShadow: '0 4px 32px rgba(137,194,217, 0.2)',
         transition: 'transform 0.3s ease-in-out, box-shadow 0.3s ease-in-out',
-        mx: '1.5rem',
-        mb: '1.5rem',
+        m: '2.5rem',
         '&:hover': {
           transform: 'translateY(-5px)',
           boxShadow: '0 8px 16px rgba(137,194,217, 0.4)',
         },
       }}
       onClick={() => onClick(country)}>
-      <CardMedia component='img' sx={{ width: '100%', height: '175px', objectFit: 'cover' }} image={country.flags} alt={`${country.name} flag`} />
+        <CardMedia
+          component='img'
+          sx={{ aspectRatio: '16/9', objectFit: 'cover' }}
+          image={country.flags}
+          alt={`${country.name} flag`}
+        />
 
-      <CardContent sx={{ background: '#f5f5f5', padding: '24px', paddingX: '16px', paddingY: '8px' }}>
-        <Typography variant='h6' component='div' sx={{ fontWeight: 'bold', fontSize: '18px', marginBottom: '4px' }}>
+      <CardContent
+        sx={{ background: '#f5f5f5', height: '100%', padding: '10px', '&:last-child': { paddingBottom: '10px' }}}>
+        <Typography
+          variant='h6'
+          component='div'
+          sx={{ fontWeight: 'bold', fontSize: '18px', marginBottom: '4px' }}>
           {country.name}
         </Typography>
         <Box>
-          <Typography variant='body2' sx={{ fontWeight: 'bold', display: 'inline-block', fontSize: '14px' }}>
+          <Typography
+            variant='body2'
+            sx={{ fontWeight: 'bold', display: 'inline-block', fontSize: '14px' }}>
             Population:
           </Typography>
-          <Typography variant='body2' color='text.secondary' sx={{ display: 'inline-block', marginLeft: '4px', fontSize: '14px' }}>
+          <Typography
+            variant='body2'
+            color='text.secondary'
+            sx={{ display: 'inline-block', marginLeft: '4px', fontSize: '14px' }}>
             {country.population}
           </Typography>
         </Box>
         <Box>
-          <Typography variant='body2' sx={{ fontWeight: 'bold', display: 'inline-block', fontSize: '14px' }}>
+          <Typography
+            variant='body2'
+            sx={{ fontWeight: 'bold', display: 'inline-block', fontSize: '14px' }}>
             Region:
           </Typography>
-          <Typography variant='body2' color='text.secondary' sx={{ display: 'inline-block', marginLeft: '4px', fontSize: '14px' }}>
+          <Typography
+            variant='body2'
+            color='text.secondary'
+            sx={{ display: 'inline-block', marginLeft: '4px', fontSize: '14px' }}>
             {country.region}
           </Typography>
         </Box>
         <Box>
-          <Typography variant='body2' sx={{ fontWeight: 'bold', display: 'inline-block', fontSize: '14px' }}>
+          <Typography
+            variant='body2'
+            sx={{ fontWeight: 'bold', display: 'inline-block', fontSize: '14px' }}>
             Capital:
           </Typography>
-          <Typography variant='body2' color='text.secondary' sx={{ display: 'inline-block', marginLeft: '4px', fontSize: '14px' }}>
+          <Typography
+            variant='body2'
+            color='text.secondary'
+            sx={{ display: 'inline-block', marginLeft: '4px', fontSize: '14px' }}>
             {country.capital || 'N/A'}
           </Typography>
         </Box>
